9e564cd50e9646f6080cb39b33446f65396bfcd0
ShantanuGV <shantnuvispute@gmail.com>
2025-10-10 00:49:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for Vercel or static hosting
+  base: '', // DO NOT use '/', use '' to make all paths relative
 })
