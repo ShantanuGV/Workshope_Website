4a9ea36d2df20feffdf0292a07d28615c797a327
ShantanuGV <shantnuvispute@gmail.com>
2025-10-12 00:56:34 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJEPdc8jKtSvPQagvk-wOUlJTPJDoTzGc",
-  authDomain: "certificate-portal-fb4cf.firebaseapp.com",
-  projectId: "certificate-portal-fb4cf",
-  storageBucket: "certificate-portal-fb4cf.firebasestorage.app",
-  messagingSenderId: "208984684961",
-  appId: "1:208984684961:web:7e922d6a3e4eee9e7473e0",
-  measurementId: "G-BV2MR0KRV4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId
 };
 
 // Initialize Firebase
