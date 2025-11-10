81924963772c7784e411469ddef563fc574fd1e5
ShantanuGV <shantnuvispute@gmail.com>
2025-11-10 21:54:00 +0530

    Update Footer.jsx

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="app-footer">
       <div className="footer-container">
         <p>© {new Date().getFullYear()} All Rights Reserved.</p>
-        <p>Developed by <a href="https://shantanugv.github.io/Websites/" target="_blank" rel="noopener noreferrer">Shantanu Vispute</a></p>
+    
       </div>
     </footer>
   );
